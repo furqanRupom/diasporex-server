c88a58950367c8464de9929991ebe09ed72133be
NahidAhmed47 <nahidahmedsd47@gmail.com>
2024-07-29 21:42:26 +0600

    feat: validateAuthorization middleware setup in progress

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
